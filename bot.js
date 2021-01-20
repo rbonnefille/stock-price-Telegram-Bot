f055cf1b8212feed157ee08de1930d1022287b7c
Romain Bonnefille <60104678+rbonnefille@users.noreply.github.com>
2021-01-20 14:37:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const yahooStockPrices = require('yahoo-stock-prices');
 
@@ -8,11 +9,12 @@ const bot = new TelegramBot(token, {polling: true});
 bot.onText(/\/stock (.+)/, (msg, match) => {
   
   const chatId = msg.chat.id;
-  const companyStock = match[1];
+  const companyStock = match[1].toUpperCase();
 
   function getStockprice() {
     return yahooStockPrices.getCurrentPrice(companyStock);
   }
+  //TO DO, handling errors and send a message back to the user
   (async () => {
      stockPrice = (await getStockprice());
      const stockPriceString = `${companyStock} share is currently: ${stockPrice}$`;
